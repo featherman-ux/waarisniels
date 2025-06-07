6fd9314879a610cf4722cd219086fc8689681971
Niels Veerman <niels.veerman@Nielss-MacBook-Air.local>
2025-06-07 19:20:44 +0200

    Set base to / and add CNAME for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
-  base: '/waarisniels/', // make sure this matches your repo name!
+  base: '/', // because it's deployed to the root of the domain
 });
